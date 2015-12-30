7cce6f7a3e2d79d4f184190309ce7ff8d9592dfd
U-Spencer-MSI\Spencer <spencershadley@utexas.edu>
2015-12-30 16:26:56 -0600

    changed currentChar-3

--- js/autoType.js
+++ js/autoType.js
@@ -36,7 +36,7 @@ function unType() {
     setTimeout(
         function () {
             console.log("lambda");
-            dest.innerHTML = text[currentWord].substr(0, --currentChar);
+            dest.innerHTML = text[currentWord].substr(0, currentChar-3);
 
             /* currentChar = 1;
             if (++currentWord === text.length) {
