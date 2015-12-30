bc5ba28ad4361456f5a5d63df2388ff0179d7a70
U-Spencer-MSI\Spencer <spencershadley@utexas.edu>
2015-12-30 15:34:38 -0600

    fixed typo

--- js/autoType.js
+++ js/autoType.js
@@ -15,7 +15,7 @@ function type() {
             if (currentChar > text[currentWord].length) {
 
                 currentChar = 1;
-                (++currentWord === text.length) ? (currWord = 0) : --currentWord;
+                (++currentWord === text.length) ? (currentWord = 0) : --currentWord;
 
                 setTimeout("type()", 2500);
             }
