44fc864f63dbca77cfb92a6d0a5aae9a01074305
U-Spencer-MSI\Spencer <spencershadley@utexas.edu>
2015-12-31 16:57:15 -0600

    changed github link to open a new tab

--- js/autoType.js
+++ js/autoType.js
@@ -7,7 +7,7 @@ var currentWord = 0;
 var currentChar = 0;
 var dest = null;
 
-console.log("testing - 7");
+console.log("testing - 8");
 
 function type() {
     if(debug && trace) console.log("type()");
