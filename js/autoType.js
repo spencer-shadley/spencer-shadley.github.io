1d962d97a392cd4f112bcb4175f0a7d7ea35b79b
U-Spencer-MSI\Spencer <spencershadley@utexas.edu>
2015-12-30 22:58:41 -0600

    code cleanup

--- js/autoType.js
+++ js/autoType.js
@@ -1,20 +1,23 @@
+var debug = false;
+var trace = false;
+
 var text = [];
 var delay = 50;
 var currentWord = 0;
 var currentChar = 0;
 var dest = null;
 
-console.log("testing - 6");
+console.log("testing - 1");
 
 function type() {
-    console.log("type()");
+    if(debug && trace) console.log("type()");
     dest.innerHTML = text[currentWord].substr(0, ++currentChar);
     if (currentChar > text[currentWord].length) setTimeout("unType()", 1500);
     else                                        setTimeout("type()", delay);
 }
 
 function unType() {
-    console.log("unType()");
+    if (debug && trace) console.log("unType()");
 
     dest.innerHTML = text[currentWord].substr(0, currentChar);
     if (--currentChar === 0) {
@@ -28,6 +31,8 @@ function unType() {
 }
 
 function startTyping(textParam, delayParam, destinationParam) {
+    if (debug && trace) console.log("startTyping()");
+
     text = textParam;
     delay = delayParam;
     dest = document.getElementById(destinationParam);
