3d221685d8248979fa609e0d1b7d6dfe4d4889ef
U-Spencer-MSI\Spencer <spencershadley@utexas.edu>
2016-01-08 00:49:20 -0600

    moved sharingan code into function

--- js/easter.js
+++ js/easter.js
@@ -3,22 +3,23 @@ console.log("current version - 1");
 /** GISD - Sharingan **/
 $("#gisd-image").click(addSharingan());
 
-// create image
-var img = document.createElement("img");
-img.className = "img-circle img-responsive";
-img.src = "../img/sharingan/msharingan.png";
-img.alt = "Mangekyo Sharingan";
-
-// create div
-var div = document.createElement("div");
-div.className = "timeline-image";
-
-// create li
-var li = document.createElement("li");
-li.className = "timeline-inverted wow rotateIn";
-
 // add to timeline
 function addSharingan() {
+
+    // create image
+    var img = document.createElement("img");
+    img.className = "img-circle img-responsive";
+    img.src = "../img/sharingan/msharingan.png";
+    img.alt = "Mangekyo Sharingan";
+
+    // create div
+    var div = document.createElement("div");
+    div.className = "timeline-image";
+
+    // create li
+    var li = document.createElement("li");
+    li.className = "timeline-inverted wow rotateIn";
+
     div.appendChild(img);
     li.appendChild(div);
     $("#timeline-list").append(li);
