be8ec8cdabb5d48f8eaf19d98c1f8b9aa6f3fcfd
U-Spencer-MSI\Spencer <spencershadley@utexas.edu>
2016-01-08 02:07:15 -0600

    removed sharingan clicking

--- js/easter.js
+++ js/easter.js
@@ -30,27 +30,6 @@ $("#gisd-image").click(function () {
 
     // create li
     var li = document.createElement("li");
-    li.className = "timeline-inverted wow rotateIn sharingan-image";
-
-    div.appendChild(img);
-    li.appendChild(div);
-    $("#timeline-list").append(li);
-});
-
-$(".sharingan-image").click(function () {
-
-    // create image
-    var img = document.createElement("img");
-    img.className = "img-circle img-responsive";
-    img.src = "../img/sharingan/msharingan.png";
-    img.alt = "Mangekyo Sharingan";
-
-    // create div
-    var div = document.createElement("div");
-    div.className = "timeline-image";
-
-    // create li
-    var li = document.createElement("li");
     li.className = "timeline-inverted wow rotateIn";
 
     div.appendChild(img);
