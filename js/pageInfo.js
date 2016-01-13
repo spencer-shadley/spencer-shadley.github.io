7ebe0f6020b0cdae32821c47a8284252628b623d
U-Spencer-MSI\Spencer <spencershadley@utexas.edu>
2016-01-12 21:05:51 -0600

    updated site information

--- js/pageInfo.js
+++ js/pageInfo.js
@@ -1,17 +1,3 @@
-// Title
-document.title = "Spencer's Portfolio";
-
-// Description
-$('meta[name=description]').remove();
-$('head').append('<meta name="description" content="A portolio about Spencer Shadley regarding projects, resume, etc.">');
-
-// Favicon
-var link = document.createElement('link');
-link.type = 'image/x-icon';
-link.rel = 'shortcut icon';
-link.href = '../img/favicon.png';
-document.getElementsByTagName('head')[0].appendChild(link);
-
 // Preload androids
 function preloadImages() {
     var images = new Array();
